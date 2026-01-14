9dedbc22bd382d5d2fd3c8b105c5e9cd6a117fd7
ishan gauri <ishangauri506@gmail.com>
2026-01-14 19:07:39 +0530

    Add files via upload

---
+++ Add2.java
@@ -0,0 +1,12 @@
+// Write a Java program to find the sum of three numbers.
+
+public class Add2{
+    public static void main(String[] args) {
+        int a = 80;
+        int b = 50;
+        int c = 20;
+
+        int sum = a + b + c;
+        System.out.println("Sum of three number :" + sum);
+    }
+}
