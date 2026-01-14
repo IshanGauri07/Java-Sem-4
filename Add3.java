455c9dc350035fd47d4a538ccdcb1049e4d95b09
ishan gauri <ishangauri506@gmail.com>
2026-01-14 19:17:23 +0530

    Add files via upload

---
+++ Add3.java
@@ -0,0 +1,24 @@
+// Write a Java program to find the sum of three numbers by user.
+
+
+import java.util.Scanner;
+
+public class Add3{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter First Number :");
+        int a = sc.nextInt();
+
+        System.out.println("Enter Second Number :");
+        int b = sc.nextInt();
+
+        System.out.println("Enter Third Number :");
+        int c = sc.nextInt();
+
+        int sum = a + b + c;
+
+        System.out.println("Sum of three number :" + sum);
+    }
+}
