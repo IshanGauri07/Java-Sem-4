f37642c0ea1d7e352a7884df53657f186ec27ce9
ishan gauri <ishangauri506@gmail.com>
2026-02-02 23:07:10 +0530

    Add files via upload

---
+++ multiplication.java
@@ -0,0 +1,17 @@
+// Write a Java program to print a multiplication table.
+
+import java.util.Scanner;
+
+public class multiplication {
+    public static void main(String[] args) {
+        
+        Scanner sc =  new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+
+        for(int i = 0; i <= 10; i++){
+            System.out.println(n + " x " + i + " = " +(n * i));
+        }
+    }
+}
