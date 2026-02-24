2a90b4a60d02c440c2718ad703e7ade8d9306bf8
ishan gauri <ishangauri506@gmail.com>
2026-02-25 00:14:07 +0530

    Add files via upload

---
+++ StarPattern13.java
@@ -0,0 +1,36 @@
+public class StarPattern13{
+
+	public static void main(String[] args) {
+		int rows = 5;
+		
+		for (int i = rows; i >= 1; i--) {
+			for (int j = 1; j <= rows-i; j++) {
+				System.out.print("  ");
+			}
+			
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+		}
+		
+		for (int j = 1; j <= rows - 1; j++) {
+            System.out.print("  ");
+        }
+		System.out.println("*");
+
+		
+		for(int i = 2; i <= rows; i++) {
+			for(int j = 1; j <= rows - i; j++) {
+				System.out.print("  ");
+			}
+			
+			for(int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+		}
+	}
+}
