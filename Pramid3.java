c8ce1075857d49df32a40657c959916f2725548e
ishan gauri <ishangauri506@gmail.com>
2026-02-26 12:20:53 +0530

    Add files via upload

---
+++ Pramid3.java
@@ -0,0 +1,19 @@
+public class Pramid3 {
+
+	public static void main(String[] args) {
+		int rows = 5;
+		
+		for (int i = 1; i <= rows; i++) {
+			for (int j = 1; j < i; j++) {
+				System.out.print(" ");
+			}
+			for ( int j = i; j <= rows; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+} 
+
